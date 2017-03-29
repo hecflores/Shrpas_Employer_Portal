04a5238a0a1007f13b282f5b73281149dd4dfefd
Hector Flores <Hector Flores>
2017-03-29 18:48:28 -0500

    Oops...

--- src/main/webapp/src/test/app.spec.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Hector on 2/28/2017.
- */
-
-describe('app', function () {
-    it('loads without problems', function () {
-        require('../app');
-    });
-});
--- src/main/webapp/start_react.cmd
+++
@@ -1,7 +0,0 @@
-echo off
-echo /********************************************************/
-echo /* Starting Watch of React Code                         */
-echo /*                                                      */
-echo /* Close if you want to stop this                       */
-echo /********************************************************/
-.\node_modules\.bin\webpack main.js
