b7bf8764f3dad241c1d791c4fccf5d8d44ecae7a
Hector Flores <Hector Flores>
2017-03-29 18:19:57 -0500

    Changed start_react

--- start_react.cmd
+++ start_react.cmd
@@ -5,5 +5,5 @@ echo /* Starting Watch of React Code                         */
 echo /*                                                      */
 echo /* Close if you want to stop this                       */
 echo /********************************************************/
-.\node_modules\.bin\webpack main.js
+.\node_modules\.bin\webpack src/app.js --watch
 popd
