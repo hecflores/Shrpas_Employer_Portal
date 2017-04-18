5eadcda6bb258dc889e93951c6834b38af404776
Hector Flores <hectorhpflores72@gmail.com>
2017-04-18 13:20:33 -0500

    Removing Old Source

--- src/main/java/hello/repositories/UserSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package hello.repositories;
-
-import hello.models.UserSession;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "userSessions")
-public interface UserSessionRepository extends PagingAndSortingRepository<UserSession, String>{
-}
--- src/main/java/hello/repositories/exceptions/ParticipantNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package hello.repositories.exceptions;
-
-public class ParticipantNotFoundException extends Exception {
-
-    public ParticipantNotFoundException(Integer participantID) {
-        super("Could not find participant of ID: " + participantID);
-    }
-}
