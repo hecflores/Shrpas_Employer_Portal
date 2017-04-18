c9a5a8ee2fd7b22f8d84fc78178948aa82f0f0d9
Hector Flores <hectorhpflores72@gmail.com>
2017-04-18 13:22:14 -0500

    Removing the rest

--- java/hello/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package hello.repositories;
-
-import hello.models.Role;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "roles", path = "roles")
-public interface RoleRepository extends PagingAndSortingRepository<Role, Integer> {
-}
--- java/hello/repositories/exceptions/TargetGroupNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package hello.repositories.exceptions;
-
-public class TargetGroupNotFoundException extends Exception {
-
-    public TargetGroupNotFoundException(Integer targetGroupId) {
-        super("Could not find target group of ID: " + targetGroupId);
-    }
-}
